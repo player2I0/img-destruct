30c9567028a7022e782d081c89ddb62936025225
player2I0 <59975613+player2I0@users.noreply.github.com>
2021-12-25 09:33:08 +0300

    1.0

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1640413952,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/obj-sheet0.png",
+		"images/canvas.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
